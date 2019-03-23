d88a3271e7a3668ac2556f4deadd08fcea714103
Clayton Stanley <cstanley25@bloomberg.net>
2019-03-23 15:17:58 -0400

    foo

--- analyze.R
+++ analyze.R
@@ -8,11 +8,16 @@ aTbl = data.table(rate=c(.1, .12, .22, .24, .32, .35, .37),
 
 aTbl[, .SD
      ][.(inc=0:1000000), on=.(inc), roll=Inf
+     ][, copy(.SD)[aTbl, bracketP := T, on=.(inc)]
+     ][inc == 0, bracketP := NA
      ][, inc := inc + 1
      ][, rateMean := cummean(rate)
-     ][sample(.N, 10000)
+     ][, rowid := 1:.N
+     ][rowid %in% sample(.N, 10000) | bracketP
      ][, ggplot(.SD, aes(inc, rateMean))
      + geom_line()
+     + geom_point(data=.SD[bracketP == T])
+     + geom_point(data=.SD[bracketP == T], aes(inc, rate), color='blue')
      + xlab('Income')
      + ylab('Average Tax')
      + scale_x_continuous(label=comma, breaks=pretty_breaks(n=10))
